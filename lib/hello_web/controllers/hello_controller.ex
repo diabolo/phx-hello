26b5e06ca31ce03402b6ee75afd25d8121c5414a
Andrew Premdas <apremdas@yahoo.co.uk>
2017-08-22 15:23:17 +0100

    Another page
    note:
    
    - don't have to change the view, only have to add a template
    - pattern matching in controller#show method params
    - is this safe from js injection (seems to be)

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -5,5 +5,9 @@ defmodule HelloWeb.HelloController do
 
     render conn, "index.html"
   end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render conn, "show.html", messenger: messenger
+  end
 end
 
